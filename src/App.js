674ba25fbc638a4e1065625d5731d2890df0a975
TliliFadhila <ziiwii40@gmail.com>
2021-05-28 10:28:39 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from 'react';
+// import { BrowserRouter as Router, Route} from "react-router-dom";
 import './App.css';
+import AddMovie from "./component/AddMovie";
+import Filter from "./component/Filter";
+import MovieList from "./component/MovieList";
+import Description from './component/Description'
+import { moviedata } from './data';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 
 function App() {
+const [movieList , setMovieList ] = useState(moviedata)
+const [title, setTitle] = useState("")
+const [rate, setRate] = useState(0)
+
+const handleChange = (e) => {
+  setTitle(e.target.value);
+};
+
+const ratingChanged = (newRating) => {
+  setRate(newRating)
+};
+const handleAdd = (newMovie) => {
+  setMovieList([...MovieList, newMovie])
+};
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+      <Filter handleChange={handleChange} title ratingChanged={ratingChanged}/>
+      <Route path="/" exact render={() =>
+      <MovieList
+      movieList={movieList.filter((movie)=>
+        movie.title.trim().toUpperCase().includes(title.toUpperCase()) && movie.rate>=rate
+        )}
+      />}/>
+      
+      <AddMovie handleAdd={handleAdd}/>
+      <Route
+            path="/movieCard/:id"
+            render={(props) => <Description {...props} movieList={movieList} exact/>}
+          />
+      </Router>
     </div>
+
   );
 }
 
---
+++ src/component/AddMovie.js
@@ -0,0 +1,85 @@
+import React, { useState } from 'react'
+import { Button, Modal } from 'react-bootstrap';
+
+
+const AddMovie = ({handleAdd}) => {
+const [title, settitle] = useState("")
+const [url, seturl] = useState("")
+const [rate, setRate] = useState(0)
+const [show, setShow] = useState(false)
+
+
+const handleClose = () => setShow(false);
+const handleShow = () => setShow(true);
+const handleSubmit = (e) => {
+    e.preventDefault();
+    const newMovie = {
+        id:Math.random(),
+        title,
+        posterUrl:url,
+        rate
+    }
+    if (title && url && rate){
+        handleAdd(newMovie)
+        setRate()
+        settitle('')
+        seturl("")
+        handleClose()
+    }
+    else alert("Missing fields")
+}
+
+return (
+  <>
+    <Button variant="primary" onClick={handleShow}>
+      Add Movie
+    </Button>
+
+    <Modal show={show} onHide={handleClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>New Movie</Modal.Title>
+      </Modal.Header>
+      <form onSubmit={handleSubmit}>
+        <Modal.Body>
+          <label htmlFor="title">Title</label>
+          <input
+            type="text"
+            name="title"
+            id="title"
+            className="form-control"
+            onChange={e => settitle(e.target.value)}
+          />
+
+          <label htmlFor="image">Image</label>
+          <input
+            type="url"
+            name="image"
+            id="image"
+            className="form-control"
+            onChange={e => seturl(e.target.value)}
+
+          />
+          <label htmlFor="rate">Rate</label>
+          <input
+            type="number"
+            name="rate"
+            id="rate"
+            className="form-control"
+            onChange={e => setRate(e.target.value)}
+          />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" type="submit">
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </form>
+    </Modal>
+  </>
+);
+};
+
+export default AddMovie;
